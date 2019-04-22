32e92157fdede4c40616dcc09b3df82ccc03508f
Olha Wysocky and Tanner Damron <olha@>
2019-04-22 16:43:24 -0700

    add loop

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,12 +1,36 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Ticket from './Ticket';
 
+let masterTicketList = [
+  {
+    names: 'Thato and Haley',
+    location: '3A',
+    issue: 'Firebase won\'t save records'
+  },
+  {
+    names: 'Sleater and Kinney',
+    location: '4B',
+    issue: 'Image not displaying'
+  },
+  {
+    names: 'Imani and Jacob',
+    location: '9F',
+    issue: 'No workyyyyyyyy'
+  }
+];
+
 function TicketList(){
   return (
-    <Ticket
-    location="3A"
-    names="Thato and Haley"
-    issue="Firebase will not save record"/>
+    <div>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index}/>
+      )}
+    </div>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,24 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 
 const render = (Component) => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component/>
-        </AppContainer>,
-        document.getElementById('react-app-root')
-    );
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('react-app-root')
+  );
 };
 
 render(App);
 
 if(module.hot) {
-    module.hot.accept('./components/App', () => {
-        render(App)
-    });
+  module.hot.accept('./components/App', () => {
+    render(App);
+  });
 }
 
